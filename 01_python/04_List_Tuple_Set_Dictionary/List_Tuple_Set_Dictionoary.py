aeaea09bccd31db66328c847cf6ddc732165cec2
aminizahra <amini.zahra@hotmail.com>
2023-02-19 20:27:56 +0330

    Add Touple

---
+++ 01_python/04_List_Tuple_Set_Dictionary/List_Tuple_Set_Dictionoary.py
@@ -0,0 +1,13 @@
+#Touple
+flower = ('Touple', )
+print(type(flower))
+
+marks1 = ('A', 'B', 'C', 'D')
+
+marks2 = ('W', 'X', 'Y', 'Z') # You can change all members of the touple
+
+#print(marks)
+
+all_marks = marks1 + marks2
+
+print(all_marks)
